82f30da3b4404256dac9a5d6ddb4f204d25ad9ca
Asiyanaveed <160754902+Asiyanaveed@users.noreply.github.com>
2024-03-01 17:35:39 +0500

    Add files via upload

---
+++ task14/14th.js
@@ -0,0 +1,4 @@
+var guest_list = ['Bilal', 'Owais', 'faisal', 'fahim'];
+for (var i = 0; i < guest_list.length; i++) {
+    console.log('Respected Brother' + guest_list[i] + ',\n We invited you on dinner today.\nThank you\n');
+}
---
+++ task14/14th.ts
@@ -0,0 +1,4 @@
+let guest_list: string[]=['Bilal','Owais','faisal','fahim'];
+for(let i=0; i<guest_list.length;i++){
+    console.log('Respected Brother'+ guest_list[i]+ ',\n We invited you on dinner today.\nThank you\n')
+}
